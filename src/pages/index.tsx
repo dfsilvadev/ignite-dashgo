869a040a68d48db445a424f11961155a17894495
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-02 18:01:27 -0300

    feat: implementando a page Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,62 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+import { TiChevronRight } from "react-icons/ti";
+
 export default function Home() {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <Flex width="100vw" height="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        background="gray.800"
+        padding={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              type="text"
+              name="email"
+              id="email"
+              size="lg"
+              background="gray.900"
+              variant="filled"
+              focusBorderColor="pink.500"
+              _hover={{ bgColor: "gray.900" }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              size="lg"
+              background="gray.900"
+              variant="filled"
+              focusBorderColor="pink.500"
+              _hover={{ bgColor: "gray.900" }}
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            rightIcon={<TiChevronRight />}
+            colorScheme="pink"
+            size="lg"
+          >
+            Enter
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   );
 }
