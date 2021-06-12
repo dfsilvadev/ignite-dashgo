7505baa211fded9f8f34f4b13ed0b3cf97443d9f
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-12 11:43:58 -0300

    feat: subistituindo a requisição fetch para axio

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -30,13 +30,13 @@ import { RiDeleteBin5Line, RiEdit2Line, RiMore2Fill } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
 
       const users = data.users.map((user) => {
         return {
